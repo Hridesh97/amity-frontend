74cd783bd82857c0858d198a46be78a9be4c9c0a
hkharry97 <hrideshharry@gmail.com>
2026-03-01 09:28:06 +0530

    fix case sensitive path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Hero from '../components/home/Hero';
-import Services from '../components/home/Services';
-import WhyChooseUs from '../components/home/WhyChooseUs';
-import PortfolioPreview from '../components/home/PortfolioPreview';
-import Testimonials from '../components/home/Testimonials';
-import CTA from '../components/home/CTA';
+import Hero from '../components/Home/Hero';
+import Services from '../components/Home/Services';
+import WhyChooseUs from '../components/Home/WhyChooseUs';
+import PortfolioPreview from '../components/Home/PortfolioPreview';
+import Testimonials from '../components/Home/Testimonials';
+import CTA from '../components/Home/CTA';
 
 const Home = () => {
   return (
